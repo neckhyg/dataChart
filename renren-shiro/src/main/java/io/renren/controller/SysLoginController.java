26c169535dff3927b97725acb59b4ccffe5d092e
neckhyg <neckx@sohu.com>
2017-07-26 10:35:47 +0800

    czit

--- renren-shiro/src/main/java/io/renren/controller/SysLoginController.java
+++ renren-shiro/src/main/java/io/renren/controller/SysLoginController.java
@@ -1,21 +1,11 @@
 package io.renren.controller;
 
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.Producer;
+import io.renren.entity.SysUserEntity;
 import io.renren.utils.R;
 import io.renren.utils.ShiroUtils;
-
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.code.kaptcha.Constants;
-import com.google.code.kaptcha.Producer;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * 登录相关
@@ -61,16 +55,18 @@ public class SysLoginController {
 	@ResponseBody
 	@RequestMapping(value = "/sys/login", method = RequestMethod.POST)
 	public R login(String username, String password, String captcha)throws IOException {
+        Subject subject;
 		String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
 		if(!captcha.equalsIgnoreCase(kaptcha)){
 			return R.error("验证码不正确");
 		}
 		
 		try{
-			Subject subject = ShiroUtils.getSubject();
+			 subject = ShiroUtils.getSubject();
 			//sha256加密
 			password = new Sha256Hash(password).toHex();
 			UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+
 			subject.login(token);
 		}catch (UnknownAccountException e) {
 			return R.error(e.getMessage());
@@ -81,8 +77,17 @@ public class SysLoginController {
 		}catch (AuthenticationException e) {
 			return R.error("账户验证失败");
 		}
-	    
-		return R.ok();
+        SysUserEntity user   = (SysUserEntity)ShiroUtils.getUserEntity();
+
+        Long userId = user.getUserId();
+        //系统管理员，拥有最高权限
+        if(userId == 1){
+            return R.ok("admin");
+        }else{
+            return R.ok("user");
+        }
+//		return R.ok();
+//        return R.ok().put("userId", userId);
 	}
 	
 	/**
