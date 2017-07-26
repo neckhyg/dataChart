2edc14a153221e1cb5d40808d47ba9b0125da51b
neckhyg <neckx@sohu.com>
2017-07-26 10:13:09 +0800

    czit

---
+++ renren-web/src/main/java/io/renren/service/impl/CzitClassServiceImpl.java
@@ -0,0 +1,53 @@
+package io.renren.service.impl;
+
+import io.renren.dao.CzitClassDao;
+import io.renren.entity.CzitClassEntity;
+import io.renren.service.CzitClassService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Service("czitClassService")
+public class CzitClassServiceImpl implements CzitClassService {
+	@Autowired
+	private CzitClassDao czitClassDao;
+	
+	@Override
+	public CzitClassEntity queryObject(Integer id){
+		return czitClassDao.queryObject(id);
+	}
+	
+	@Override
+	public List<CzitClassEntity> queryList(Map<String, Object> map){
+		return czitClassDao.queryList(map);
+	}
+	
+	@Override
+	public int queryTotal(Map<String, Object> map){
+		return czitClassDao.queryTotal(map);
+	}
+	
+	@Override
+	public void save(CzitClassEntity czitClass){
+		czitClassDao.save(czitClass);
+	}
+	
+	@Override
+	public void update(CzitClassEntity czitClass){
+		czitClassDao.update(czitClass);
+	}
+	
+	@Override
+	public void delete(Integer id){
+		czitClassDao.delete(id);
+	}
+	
+	@Override
+	public void deleteBatch(Integer[] ids){
+		czitClassDao.deleteBatch(ids);
+	}
+	
+}
