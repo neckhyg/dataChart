65d13c030ccd649faea8f5a5f12d57f7b887a8dd
neckhyg <100001195333@git.cloud.tencent.com>
2017-09-27 17:25:05 +0800

    add student Card

---
+++ renren-web/src/main/java/io/renren/dao/StudentCardDao.java
@@ -0,0 +1,14 @@
+package io.renren.dao;
+
+import io.renren.entity.StudentCardEntity;
+
+/**
+ * 
+ * 
+ * @author neckhyg
+ * @email 497273569@qq.com
+ * @date 2017-09-26 15:16:32
+ */
+public interface StudentCardDao extends BaseDao<StudentCardEntity> {
+	
+}
--- renren-web/src/main/webapp/js/sys/menu.js
+++ renren-web/src/main/webapp/js/sys/menu.js
@@ -25,9 +25,9 @@ $(function () {
 			{ label: '排序号', name: 'orderNum', index: "order_num", width: 50}
         ],
 		viewrecords: true,
-        height: 385,
-        rowNum: 10,
-		rowList : [10,30,50],
+        height: 485,
+        rowNum: 50,
+		rowList : [50,100,200],
         rownumbers: true, 
         rownumWidth: 25, 
         autowidth:true,
